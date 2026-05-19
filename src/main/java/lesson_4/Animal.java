4d0460a3134c560eb88f7dd2e8398c63c3e264a7
Lintervil <parfenovignat@gmail.com>
2026-05-19 12:49:18 +0300

    lesson_4: completed homework - Animals + Shapes with interfaces

---
+++ src/main/java/lesson_4/Animal.java
@@ -0,0 +1,22 @@
+package lesson_4;
+
+public abstract class Animal {
+    protected String name;
+    private static int animalCount = 0;
+
+    public Animal(String name) {
+        this.name = name;
+        animalCount++;
+    }
+
+    public abstract void run(int distance);
+    public abstract void swim(int distance);
+
+    public static int getAnimalCount() {
+        return animalCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/lesson_4/Bowl.java
@@ -0,0 +1,28 @@
+package lesson_4;
+
+public class Bowl {
+    private int foodAmount;
+
+    public Bowl(int foodAmount) {
+        this.foodAmount = Math.max(0, foodAmount);
+    }
+
+    public boolean decreaseFood(int amount) {
+        if (amount > 0 && foodAmount >= amount) {
+            foodAmount -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void addFood(int amount) {
+        if (amount > 0) {
+            foodAmount += amount;
+            System.out.println("В миску добавлено " + amount + " еды. Теперь: " + foodAmount);
+        }
+    }
+
+    public int getFoodAmount() {
+        return foodAmount;
+    }
+}
